acd0f0e797ccb397af2f2e723b26f7477f13687a
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2022-02-07 21:17:32 +0300

    add transition to view present

--- SwiftUIProgrammingGraphics/ContentView.swift
+++ SwiftUIProgrammingGraphics/ContentView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSmile = false
+    
     var body: some View {
-        SmileView()
+        
+        VStack {
+            Spacer()
+            if showSmile {
+                SmileView()
+                    .frame(width: 250, height: 250)
+                    .transition(showSmile ? .slide: .leadingSlide)
+            }
+            Spacer()
+            
+            Button(action: buttonAction) {
+                Text(showSmile ? "Hide Smile" : "Smile")
+            }
+        }
+    }
+    
+    private func buttonAction() {
+        withAnimation {
+            showSmile.toggle()
+        }
+    }
+}
+
+extension AnyTransition {
+    static var leadingSlide: AnyTransition {
+        let insertion = AnyTransition.move(edge: .leading)
+            .combined(with: .scale)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
     }
 }
 
